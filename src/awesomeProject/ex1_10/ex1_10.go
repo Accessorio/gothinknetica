01ae4cdfefd894d5c4adb55c3a9970490b26d82e
Accessorio <egor@4e4el.ru>
2023-06-26 17:19:57 +0300

    ver1

---
+++ src/awesomeProject/ex1_10/ex1_10.go
@@ -0,0 +1,61 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 17.
+//!+
+
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		if !(strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")) {
+			url = "http://" + url
+		}
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(uri string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(uri)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	f, err := os.Create(url.QueryEscape(uri))
+	if err != nil {
+		ch <- err.Error()
+	}
+
+	nbytes, err := io.Copy(f, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", uri, err)
+		return
+	}
+	if closeErr := f.Close(); err == nil { // example from chapter 5.8, gopl.io/ch5/fetch
+		err = closeErr
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, uri)
+}
+
+//!-
