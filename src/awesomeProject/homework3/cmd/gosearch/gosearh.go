c3f5032eef3def7ac474e453b65e0fe5c9dccb5a
Accessorio <egor@4e4el.ru>
2023-08-03 14:03:53 +0300

    hw3

---
+++ src/awesomeProject/homework3/cmd/gosearch/gosearh.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go-core-4/homework3/pkg/crawler"
+	"go-core-4/homework3/pkg/crawler/spider"
+	"go-core-4/homework3/pkg/index"
+	"go-core-4/homework3/pkg/search"
+)
+
+func main() {
+	f := flag.String("s", "", "word")
+	flag.Parse()
+	var id int
+	var b []crawler.Document
+	var array = [...]string{"https://go.dev", "https://golang.org"}
+	for _, link := range array {
+		s := spider.New()
+		d, err := s.Scan(link, 2, &id)
+		if err != nil {
+			fmt.Println(err)
+		}
+		b = append(b, d...)
+
+	}
+	idb := index.Indexing(b)
+	x := search.Searching(idb, f)
+	for _, id := range x {
+		val := b[id]
+		fmt.Println("id:", id, val.Title, "\n", val.URL)
+	}
+}
---
+++ src/awesomeProject/homework3/pkg/index/index.go
@@ -0,0 +1,32 @@
+package index
+
+import (
+	"fmt"
+	"go-core-4/homework3/pkg/crawler"
+	"strings"
+	"unicode"
+)
+
+type Index map[string][]int
+
+func Indexing(a []crawler.Document) Index {
+	idb := make(Index)
+	for _, c := range a {
+		w := SToWords(strings.ToLower(c.Title))
+		for _, v := range w {
+			val := idb[v]
+			if val != nil && val[len(val)-1] == c.ID {
+				continue
+			}
+			idb[v] = append(val, c.ID)
+		}
+	}
+	fmt.Println(idb)
+	return idb
+}
+
+func SToWords(s string) []string {
+	return strings.FieldsFunc(s, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+}
